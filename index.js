3721a3288dd5478a5f1802e28531bba9b7a70eb1
Inglish Foust <99756614+inglish2022@users.noreply.github.com>
2022-04-23 16:42:04 -0400

    html practice

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+
+const profileDataArgs = process.argv.slice(2);
+const [projectTitle, github] = profileDataArgs;
+
+
+
 // TODO: Include packages needed for this application
 
+fs.writeFile('./index.html', generatePage(projectTitle, github), err =>  {
+    if (err) throw err;
+
+    console.log('ReadMe complete! Check out index.html')
+})
+
+
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
+
+
