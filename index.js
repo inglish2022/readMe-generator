d6f91a84aede9c67b50271a03303d5527f3a84a9
Inglish Foust <99756614+inglish2022@users.noreply.github.com>
2022-04-23 18:05:08 -0400

    added questions

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
+const inquirer = require('inquirer');
 const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+
+const generatePage = require('./utils/generateMarkdown.js');
+
+const questions = () => {
+    return inquirer.prompt([
+        {
+
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?'
+        },
+        {
+            type: 'input',
+            name: 'Title',
+            message: 'What is your Project title?'
+        },
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'Provide a short description of your project.'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)'
+        },
+    ]);
+};
+questions().then(answers => console.log(answers));
 
 
-const profileDataArgs = process.argv.slice(2);
-const [projectTitle, github] = profileDataArgs;
+
+// const pageHTML = generatePage(projectTitle, github);
+
 
 
 
 // TODO: Include packages needed for this application
 
-fs.writeFile('./index.html', generatePage(projectTitle, github), err =>  {
-    if (err) throw err;
+// fs.writeFile('./index.html', generatePage(projectTitle, github), err =>  {
+    // if (err) throw err;
 
-    console.log('ReadMe complete! Check out index.html')
-})
+    // console.log('ReadMe complete! Check out index.html')
+// })
 
 
 // TODO: Create an array of questions for user input
