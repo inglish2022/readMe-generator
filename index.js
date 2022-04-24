ac05913a222fb2c90478e6b6f9e1b3d60d9df7ea
Inglish Foust <99756614+inglish2022@users.noreply.github.com>
2022-04-23 20:55:28 -0400

    starting program

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
 const generatePage = require('./utils/generateMarkdown.js');
 
+
+// TODO: Create an array of questions for user input
 const questions = () => {
     return inquirer.prompt([
         {
@@ -92,17 +95,30 @@ const questions = () => {
         },
     ]);
 };
-questions().then(answers => console.log(answers));
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+
+
+
+// TODO: Create a function to initialize app
+questions().then(answers => {
+    return generatePage(answers);
+})
+
+
+
+// TODO: Create a function to initialize app
+// function init() {}
+
+// Function call to initialize app
+// init();
+
+
 
 
 
 // const pageHTML = generatePage(projectTitle, github);
-
-
-
-
-// TODO: Include packages needed for this application
-
 // fs.writeFile('./index.html', generatePage(projectTitle, github), err =>  {
     // if (err) throw err;
 
@@ -110,16 +126,9 @@ questions().then(answers => console.log(answers));
 // })
 
 
-// TODO: Create an array of questions for user input
-// const questions = [];
 
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-// function init() {}
 
-// Function call to initialize app
-// init();
+
 
 
